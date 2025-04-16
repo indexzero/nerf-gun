25883465fbce13dce3d9e74afbb18d8859362a9b
Charlie Robbins <charlie.robbins@gmail.com>
2025-04-16 00:56:30 -0500

    [refactor breaking] v2 from latest (#1)

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var url = require("url")
-
-module.exports = toNerfDart
+/**
+ * (v1) Adapted under the ISC License from `npm`, `npm-registry-client`
+ *   - https://github.com/npm/npm-registry-client/blob/master/lib/util/nerf-dart.js
+ *   - https://github.com/npm/npm/blob/master/lib/config/nerf-dart.js
+ *
+ * (v2) Adapted under the ISC License from `npm`, `npm/cli`
+ *   - https://github.com/npm/cli/blob/latest/workspaces/config/lib/nerf-dart.js
+ */
+const { URL } = require('node:url')
 
 /**
  * Maps a URL to an identifier.
- *
- * Adapted under the ISC License from `npm` and `npm-registry-client`
- *   - https://github.com/npm/npm-registry-client/blob/master/lib/util/nerf-dart.js
- *   - https://github.com/npm/npm/blob/master/lib/config/nerf-dart.js
  *
  * Name courtesy schiffertronix media LLC, a New Jersey corporation
  *
@@ -15,14 +17,8 @@ module.exports = toNerfDart
  *
  * @returns {String} A nerfed URL.
  */
-function toNerfDart(uri) {
-  var parsed = url.parse(uri)
-  parsed.pathname = "/"
-  delete parsed.protocol
-  delete parsed.auth
-  delete parsed.query
-  delete parsed.search
-  delete parsed.hash
-
-  return url.format(parsed)
+module.exports = (url) => {
+  const parsed = new URL(url)
+  const res = `//${parsed.host}${parsed.pathname}/`
+  return res.replace(/\/\/$/g, '/');
 }
