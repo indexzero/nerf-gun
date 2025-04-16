4035d58f1d9bcc0084db5790e2168ba572b87ac8
indexzero <charlie.robbins@gmail.com>
2025-04-16 01:55:43 -0400

    [refactor breaking] v2 from latest

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ A tiny utility that returns nerf darts from URLs for npm.
 Give it a URL and you'll get a "nerf dart":
 
 ``` js
-var nerfGun = require('nerf-gun')
+const nerfGun = require('nerf-gun');
 
 //
 // All of these return the same thing
@@ -27,6 +27,8 @@ nerfGun('http://registry.npmjs.org/some-package#random-hash')
 ##### LICENSE
 
 Copyright (c) 2014, Charlie Robbins and Contributors. Adapted under the ISC License from [`npm`](https://github.com/npm/npm/blob/v2.1.5/lib/config/nerf-dart.js) and [`npm-registry-client`](https://github.com/npm/npm-registry-client/blob/v3.2.4/lib/util/nerf-dart.js)
+
+Copyright (c) 2025, Charlie Robbins and Contributors. Adapted under the Art License from `npm` via [`npm/cli`](https://github.com/npm/cli/blob/latest/workspaces/config/lib/nerf-dart.js)
 
  [Charlie Robbins]: https://github.com/indexzero
  [Forrest Norvell]: https://github.com/othiym23
--- test/nerf-gun-test.js
+++ test/nerf-gun-test.js
@@ -1,6 +1,6 @@
 
-var tap = require("tap")
-var toNerfDart = require('../');
+const tap = require('tap');
+const toNerfDart = require('../');
 
 function validNerfDart (uri, valid) {
   tap.test(uri, function (t) {
@@ -10,11 +10,24 @@ function validNerfDart (uri, valid) {
   });
 }
 
-var valid = '//registry.npmjs.org/'
+const validRoot = '//registry.npmjs.org/';
+const validPath = '//your.private.registry/some-path/';
+const validGar  = '//your.private.registry/project/repository/';
 
-validNerfDart('http://registry.npmjs.org', valid);
-validNerfDart('http://registry.npmjs.org/some-package', valid);
-validNerfDart('http://registry.npmjs.org/some-package?write=true', valid);
-validNerfDart('http://user:pass@registry.npmjs.org/some-package?write=true', valid);
-validNerfDart('http://registry.npmjs.org/#random-hash', valid);
-validNerfDart('http://registry.npmjs.org/some-package#random-hash', valid);
+validNerfDart('http://registry.npmjs.org', validRoot);
+validNerfDart('http://registry.npmjs.org/', validRoot);
+validNerfDart('http://registry.npmjs.org?write=true', validRoot);
+validNerfDart('http://registry.npmjs.org/#random-hash', validRoot);
+validNerfDart('http://user:pass@registry.npmjs.org/?write=true', validRoot);
+
+validNerfDart('http://your.private.registry/some-path', validPath);
+validNerfDart('http://your.private.registry/some-path/', validPath);
+validNerfDart('http://your.private.registry/some-path?write=true', validPath);
+validNerfDart('http://your.private.registry/some-path#random-hash', validPath);
+validNerfDart('http://user:pass@your.private.registry/some-path?write=true', validPath);
+
+validNerfDart('http://your.private.registry/project/repository', validGar);
+validNerfDart('http://your.private.registry/project/repository/', validGar);
+validNerfDart('http://your.private.registry/project/repository?write=true', validGar);
+validNerfDart('http://your.private.registry/project/repository#random-hash', validGar);
+validNerfDart('http://user:pass@your.private.registry/project/repository?write=true', validGar);
